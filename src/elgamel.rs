9f58f39e5bf06c21e526ace42611d58e3f9ec4b4
ikanago <ikanago@ikanago.dev>
2021-03-06 16:27:40 +0900

    Define primitive utilities about prime number

---
+++ src/elgamel.rs
@@ -0,0 +1,9 @@
+/*
+pub fn generate_key(bits: u8) -> (u64, u64, u64) {
+}
+*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
+pub mod elgamel;
+
+use rand::{thread_rng, Rng};
+
+/// Determine `n` is prime number.
+pub fn is_prime(n: u64) -> bool {
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
+/// Generate specified bits prime number.
+pub fn generate_prime(bits: u8) -> u64 {
+    if bits == 0 {
+        panic!("Number of bits should be non-zero");
+    }
+
+    let mut rng = thread_rng();
+    let lower_bound = 1 << (bits - 1);
+    let upper_bound = 1 << bits;
+    loop {
+        let n = rng.gen_range(lower_bound..upper_bound);
+        if is_prime(n) {
+            return n;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn prime() {
+        assert!(is_prime(1e9 as u64 + 7));
+    }
+
+    #[test]
+    fn get_prime() {
+        let n = generate_prime(16);
+        assert!(is_prime(n));
     }
 }
