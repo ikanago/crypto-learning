d31bd187ab899357323d8c5784a9dc1c5bacf1d8
ikanago <ikanago@ikanago.dev>
2021-03-07 12:50:35 +0900

    Wip: PhantomData has modulus

--- src/field.rs
+++ src/field.rs
@@ -1,91 +1,120 @@
-use num_bigint::BigUint;
+use lazy_static::lazy_static;
+use num_bigint::{BigUint, ToBigUint};
+use std::cell::RefCell;
+use std::marker::PhantomData;
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::rc::Rc;
+use std::sync::RwLock;
 
-/// Galois field with `order`.
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Field {
-    value: BigUint,
-    order: BigUint,
+thread_local! {
+    static MODULUS: Rc<RefCell<BigUint>> = Rc::new(RefCell::new(2.to_biguint().unwrap()));
 }
 
-impl Field {
-    pub fn new(value: BigUint, order: BigUint) -> Self {
+pub type Field = GaloisField<DefaultModulus>;
+
+/// Galois field with `order`. In other words, ℤ/mℤ.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GaloisField<M> {
+    value: BigUint,
+    phantom: PhantomData<M>,
+}
+
+impl<M: Modulus> GaloisField<M> {
+    pub fn new(value: BigUint) -> Self {
+        // let value = value % *Self::modulus().borrow();
         Self {
-            value: value % &order,
-            order,
+            value,
+            phantom: PhantomData,
         }
     }
 
-    fn assert_order(&self, rhs: &Self) -> bool {
-        self.order == rhs.order
+    pub fn modulus() -> Rc<RefCell<BigUint>> {
+        M::modulus()
+    }
+
+    pub fn update_modulus(modulus: BigUint) {
+        M::update(modulus)
     }
 }
 
-impl Add for Field {
+trait Modulus {
+    fn modulus() -> Rc<RefCell<BigUint>>;
+    fn update(modulus: BigUint);
+}
+
+#[derive(Clone, Debug)]
+pub struct DefaultModulus;
+
+impl Modulus for DefaultModulus {
+    fn modulus() -> Rc<RefCell<BigUint>> {
+        MODULUS.with(|rc| rc.clone())
+    }
+
+    fn update(modulus: BigUint) {
+        MODULUS.with(|m| *m.borrow_mut() = modulus);
+    }
+}
+
+impl<M: Modulus> Add for GaloisField<M> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        assert!(self.assert_order(&rhs));
         let mut value = self.value + rhs.value;
-        if value >= self.order {
-            value -= &self.order;
+        if value >= *Self::modulus().borrow() {
+            value -= *Self::modulus().borrow();
         }
-        Field {
+        GaloisField {
             value,
-            order: self.order,
+            phantom: PhantomData,
         }
     }
 }
 
-impl AddAssign for Field {
+impl<M: Modulus> AddAssign for GaloisField<M> {
     fn add_assign(&mut self, rhs: Self) {
-        assert!(self.assert_order(&rhs));
         self.value += rhs.value;
-        if self.value >= self.order {
-            self.value -= &self.order;
+        if self.value >= *Self::modulus().borrow() {
+            self.value -= *Self::modulus().borrow();
         }
     }
 }
 
-impl Sub for Field {
+/*
+impl<M: Modulus> Sub for GaloisField<M> {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
-        assert!(self.assert_order(&rhs));
         let value = if self.value < rhs.value {
-            &self.order + self.value - rhs.value
+            Self::modulus() + self.value - rhs.value
         } else {
             self.value - rhs.value
         };
-        Field {
+        GaloisField {
             value,
-            order: self.order,
+            phantom: PhantomData,
         }
     }
 }
 
-impl SubAssign for Field {
+impl<M: Modulus> SubAssign for GaloisField<M> {
     fn sub_assign(&mut self, rhs: Self) {
-        assert!(self.assert_order(&rhs));
         if self.value < rhs.value {
-            self.value += &self.order;
+            self.value += Self::modulus();
         }
         self.value -= rhs.value;
     }
 }
+*/
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use num_bigint::ToBigUint;
 
     #[test]
     fn addition() {
-        let mut x = Field::new(3.to_biguint().unwrap(), 8.to_biguint().unwrap());
-        let y = Field::new(5.to_biguint().unwrap(), 8.to_biguint().unwrap());
+        Field::update_modulus(8.to_biguint().unwrap());
+        let mut x = Field::new(3.to_biguint().unwrap());
+        let y = Field::new(5.to_biguint().unwrap());
         let z = x.clone() + y.clone();
-        assert_eq!(
-            Field::new(0.to_biguint().unwrap(), 8.to_biguint().unwrap()),
-            z
-        );
+        assert_eq!(Field::new(0.to_biguint().unwrap()), z);
         x += y;
         assert_eq!(x, z);
     }
