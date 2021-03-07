40e7967e5cb052e0701d8d582580715915b6fdcc
ikanago <ikanago@ikanago.dev>
2021-03-07 15:53:56 +0900

    Implement generate_prime()

--- src/util.jl
+++ src/util.jl
@@ -1,13 +1,31 @@
 module Util
 
-function is_prime(n)
+using Random
+
+function is_prime(n::Integer)
+    if n < 1
+        return false
+    end
+
     i = 2
     while i * i <= n
         if n % i == 0
             return false
         end
+        i += 1
     end
     true
+end
+
+function generate_prime(bits::Integer)
+    lower_bound = 1 << (bits - 1)
+    upper_bound = 1 << bits
+    while true
+        n = rand(lower_bound:upper_bound)
+        if is_prime(n)
+            return n
+        end
+    end
 end
 
 end
--- test/test_util.jl
+++ test/test_util.jl
@@ -1,5 +1,15 @@
 using crypto_learning.Util, Test
 
-@test Util.is_prime(1)
-@test Util.is_prime(2)
-@test !Util.is_prime(4)
+@testset "Determine an integer is prime number" begin
+    @test !Util.is_prime(-1)
+    @test !Util.is_prime(0)
+    @test Util.is_prime(1)
+    @test Util.is_prime(2)
+    @test !Util.is_prime(4)
+    @test Util.is_prime(7)
+end
+
+@testset "Generate prime number" begin
+    n = Util.generate_prime(16)
+    @test Util.is_prime(n)
+end
